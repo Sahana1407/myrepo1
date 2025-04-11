4cf3aa3254ecf6a37b2b8def0f2dfc1282404c94
sahan <sahan@LAPTOP-DKAKLP3F>
2025-04-11 13:55:41 +0530

    commit 10

---
+++ Myproject2/src/com/tnsif/oops/EncapMain.java
@@ -0,0 +1,20 @@
+package com.tnsif.oops;
+
+public class EncapMain {
+	public static void main(String[] args) {
+
+		
+		
+		EncapDemo obj = new EncapDemo(); 
+		obj.setSerialNum(88);
+		obj.setName("sahana");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	
+
+
+}
+
+	
+}
